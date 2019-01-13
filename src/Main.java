ebc60d11bfae2e4e60398f3a1f1b723f5faa4996
andreig110 <42849998+andreig110@users.noreply.github.com>
2019-01-13 23:53:30 +0200

    Update src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        JButton button = new JButton("Show Fractal");
-        button.setBounds(170, 20, 140, 40);
-        frame.add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //JButton button = new JButton("Show Fractal");
+        //button.setBounds(170, 20, 140, 40);
+        //frame.add(button);
+        frame.add(new MJPanel());
         frame.setSize(480, 360);
-        frame.setLayout(null);
+        //frame.setLayout(null);
         frame.setVisible(true);
     }
 }
